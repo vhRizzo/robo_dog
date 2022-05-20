93d763274c8736b1292faf3ec361829ae2b71873
Victor Hugo Rizzo Moura <43479155+vhRizzo@users.noreply.github.com>
2022-05-20 11:02:24 -0300

    Update robo_dog.ino

--- robo_dog.ino
+++ robo_dog.ino
@@ -1,5 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include "Arduino_FreeRTOS.h"
+#include "task.h"
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -10,8 +12,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_FREQ 50
 
 uint8_t servonum = 15;
+int servo_aux[2];
 
 int position_history[12] = { 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500 };
+typedef struct {
+  int servo;
+  int final_pos;
+}servo_t;
 
 void setup() {
   Serial.begin(9600);
@@ -57,7 +64,10 @@ void setServoPulse(uint8_t n, double pulse) {
 // -90 = 500
 // +90 = 2500
 
-void use_servo(int servo, int final_pos) {
+void use_servo(void* arg) {
+  int servo = (int)(arg.servo);
+  int final_pos = int(arg[1]);
+  
   // Mapeia o servo na posicao do vetor de servos
   int servo_i = servo;
   if (servo > 5)
@@ -86,7 +96,7 @@ void use_servo(int servo, int final_pos) {
     }
     position_history[servo_i] = final_pos;
     Serial.println(position_history[servo_i]);
-    return;
+    vTaskDelete(NULL);
     
   } else if (amnt < 0) {
     amnt = amnt*-1;
@@ -96,44 +106,72 @@ void use_servo(int servo, int final_pos) {
     }
     position_history[servo_i] = final_pos;
     Serial.println(position_history[servo_i]);
-    return;
+    vTaskDelete(NULL);
     
   } else {
       Serial.println("");
-      return;
+      vTaskDelete(NULL);
   }
 }
 
 void ponta_de_pe() {
-  use_servo(0, 1150);
-  use_servo(1, 1150);
-  use_servo(4, 1850);
-  use_servo(5, 1850);
-  use_servo(11, 1850);
-  use_servo(10, 1850);
-  use_servo(14, 1150);
-  use_servo(15, 1150);
+  servo_t servo_aux;
+  
+  /* Patas */
+  servo_aux.servo = 0;
+  servo_aux.final_pos = 1300;
+  xTaskCreate(use_servo, "Servo_0", 2*1024, &servo_aux, 1, NULL);
+
+  servo_aux[0] = 5;
+  servo_aux[1] = 1700;
+  xTaskCreate(use_servo, "Servo_5", 2*1024, (void*)servo_aux, 1, NULL);
+
+  servo_aux[0] = 10;
+  servo_aux[1] = 1700;
+  xTaskCreate(use_servo, "Servo_10", 2*1024, (void*)servo_aux, 1, NULL);
+
+  servo_aux[0] = 15;
+  servo_aux[1] = 1300;
+  xTaskCreate(use_servo, "Servo_15", 2*1024, (void*)servo_aux, 1, NULL);
+  
+//  use_servo(0, 1300);
+//  use_servo(5, 1700);
+//  use_servo(10, 1700);
+//  use_servo(15, 1300);
+  
+  /* Pernas */
+//  use_servo(1, 1250);
+//  use_servo(4, 1850);
+//  use_servo(11, 1900);
+//  use_servo(14, 1250);
+  
 }
 
 void walk() {
-  use_servo(14, 1000);
-  use_servo(11, 2000);
-  use_servo(4, 1600);
-  use_servo(1, 1300);
-  
-  //ponta_de_pe();
-  //use_servo(4, 2000);
-  //use_servo(11, 1650);
-  
-  //use_servo(0, 1300);
-  //use_servo(15, 1700);
+  /* Primeiro passo */
+//  use_servo(0, position_history[0] - 300);
+//  use_servo(15, position_history[15 - 4] - 300);
+//  use_servo(5, position_history[5] - 200);
+//  use_servo(10, position_history[10 - 4] + 200);
+//  
+//  use_servo(11, position_history[11 - 4] + 300);
+//  use_servo(4, position_history[4] - 250);
+//  
+//  use_servo(10, position_history[10 - 4] - 200);
+//  use_servo(5, position_history[5] + 200);
+//
+//  use_servo(0, position_history[0] + 300);
+//  use_servo(15, position_history[15 - 4] + 300);
+//
+//  /* Recentraliza */
+//  use_servo(0, position_history[0] + 300);
+//  use_servo(15, position_history[15 - 4] + 300);
+//
+//  use_servo(5, position_history[5] + 300);
+//  use_servo(4, position_history[4] + 250);
+//  use_servo(11, position_history[11 - 4] - 300);
 
-  //ponta_de_pe();
-  //use_servo(1, 2000);
-  //use_servo(10, 1650);
   
-  //use_servo(5, 1300);
-  //use_servo(10, 1700);
 }
 
 void loop() {
